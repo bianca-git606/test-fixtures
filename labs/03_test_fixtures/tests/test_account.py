41f45565458ee852f7b1c7b313f78e853f9cd800
bianca-git606 <biancafiedalan31@yahoo.com>
2023-04-28 19:56:03 +1200

    added initial text fixtures

--- labs/03_test_fixtures/tests/test_account.py
+++ labs/03_test_fixtures/tests/test_account.py
@@ -14,16 +14,22 @@ class TestAccountModel(TestCase):
     @classmethod
     def setUpClass(cls):
         """ Connect and load data needed by tests """
+        db.create_all()
+        with open("tests/fixtures/account_data.json") as json_data:
+            ACCOUNT_DATA = json.load(json_data)
 
     @classmethod
     def tearDownClass(cls):
         """Disconnect from database"""
+        db.close()
 
     def setUp(self):
         """Truncate the tables"""
+        db.drop_all()
 
     def tearDown(self):
         """Remove the session"""
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
